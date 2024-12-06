03be5d44d12c8cd1e4224f6cc24929f16f6ab0b8
Marco Júnior <mrcez4rr@gmail.com>
2024-12-06 20:14:20 -0300

    First Template

---
+++ src/Views/commons/footer.php
@@ -0,0 +1,19 @@
+	
+
+	<footer>
+		<div class="copyright">
+			<div class="container">
+				<div class="conteudo">
+					<p>Todos direitos reservados © 2024</p>
+					<p>Desenvolvido por <a href="https://www.instagram.com/marquin.do.som/" target="_blank">Marquin.Do.Som<i class="fab fa-instagram"></i></a></p>
+				</div>
+			</div>
+		</div>
+	</footer>
+    <script src="<?=URL_BASE?>resources/js/jquery/jquery.min.js"></script>
+    <script src="<?=URL_BASE?>resources/js/slick.min.js"></script>
+    <script src="<?=URL_BASE?>resources/js/jquery.waypoints.min.js"></script>
+    <script src="<?=URL_BASE?>resources/js/jquery.swipebox.min.js"></script>
+	<script src="<?=URL_BASE?>resources/js/js.js"></script>
+</body>
+</html>
---
+++ src/Views/home.php
@@ -0,0 +1,209 @@
+<?=$this->fetch('commons/header.php', $data)?>
+<section class="curriculo">
+	<div class="container">
+		<div class="conteudo">
+			<div class="sobre-mim">
+				<div class="img">
+					<img src="<?=URL_BASE?>resources/imagens/3por4.png">
+				</div>
+				<h4>SOBRE MIM</h4>
+				<div class="descricao">
+					<p>Como estudante e futuro profissional na área de Análise e Desenvolvimento de Sistemas, tenho como objetivo me tornar um especialista no desenvolvimento de soluções tecnológicas, inovadoras e eficiêntes. Busco aplicar meus conhecimentos em programação, arquitetura de sistemas e banco de dados para criar sistemas dinâmicos, escaláveis e de alto desempenho.</p>
+					<p>Minhas metas incluem aprimorar minhas habilidades em diversas linguagens de programação, explorar as ultimas tendências em desenvolvimento de software e buscar uma constante evolução no entendimento das nessesidades do usuário final.</p>
+					<p>Estou comprometido em desenvolver soluções que otimizem processos e ofereçam resultados práticos e funcionais, alinhadas com as exigências do mercado e as melhores práticas de desenvolvimento ágil.</p>
+				</div>
+				<div class="idioma">
+					<h4>IDIOMA</h4>
+					<p>Ingles - Básico Avançado</p>
+				</div>
+			</div>
+			<div class="corpo">
+				<div class="cabeca">
+					<div class="nome">
+						<h1>Marco Antonio de Oliveira Júnior</h1>
+						<h4>Técnico e Desenvovledor</h4>
+					</div>
+					<div class="redes">
+                    	<div class="item">
+                    		<div class="icone">
+                    			<i class="fas fa-mobile-screen"></i>
+                    		</div>
+                    		<div class="info">
+                    			<h4>Telefone</h4>
+                    			<a href="tel:61981422758">+55 (61) 98142-2758</a>
+                    		</div>
+                    	</div>
+                    	<div class="item">
+                    		<div class="icone">
+                    			<i class="far fa-envelope"></i>
+                    		</div>
+                    		<div class="info">
+                    			<h4>E-mail</h4>
+                    			<a href="mailto:mrcez4rr@gmail.com" target="_blank">mrcez4rr@gmail.com</a>
+                    		</div>
+                    	</div>
+                    	<div class="item">
+                    		<div class="icone">
+                    			<i class="fas fa-location-dot"></i>
+                    		</div>
+                    		<div class="info">
+                    			<h4>Endereço</h4>
+                    			<a href="https://maps.app.goo.gl/hZXkc6GV4Af1vDXi8" target="_blank">SCLRN Bloco F Entrada 41 Apartamento 101</a>
+                    		</div>
+                    	</div>	
+					</div>
+				</div>
+				<h3>Experiência Profissional</h3>
+				<div class="experiencia-profissional">
+					<div class="dados">
+						<div class="icone">
+							<h2>Eletrônica Marquinho</h2>
+                    		<i class="fas fa-circle"></i>
+                    	</div>
+						<p>Brasília-DF</p>
+						<p>Setembro 2019 - Atual</p>
+					</div>
+					<div class="profissao">
+						<h4>Técnico em Eletrônica</h4>
+						<p>Profissional especializado em manutenção, instalação e otimização de sistemas de som automotivo. Com sólida experiência no atendimento personalizado de clientes, fornecendo soluções de alta qualidade em projetos de áudio automotivo, desde o diagnóstico de problemas até a execução de melhorias no desempenho do sistema.</p>
+						<p>Capacitado no manuseio de diversos equimpamentos e tecnologias, incluindo amplificadores, alto-falantes, subwoofer, processadores de áudio e integração de sistemas eletrônicos. Habilidade em realizar ajustes finos de áudio para proporcionar a melhor experiência sonora possível, respeitando as nessesidades e preferêcias dos clientes</p>
+					</div>
+				</div>
+				<h3>Formação</h3>
+				<div class="experiencia-profissional">
+					<div class="dados">
+						<div class="icone">
+							<h2>Gisno</h2>
+                    		<i class="fas fa-circle"></i>
+                    	</div>						
+						<p>Brasília-DF</p>
+						<p>Conclusão - 2018</p>
+					</div>
+					<div class="profissao">
+						<h4>Diploma do Ensino Médio</h4>
+						<p>Durante esse período, participei de atividades extracurriculares e trabalhei no aprimoramento de competências essenciais para minha informação pessoal e profissional.</p>
+					</div>
+				</div>
+				<div class="experiencia-profissional">
+					<div class="dados">
+						<div class="icone">
+							<h2>GRAN CURSOS</h2>
+                    		<i class="fas fa-circle"></i>
+                    	</div>						
+						<p>Brasília-DF</p>
+						<p>Conclusão em 2027</p>
+					</div>
+					<div class="profissao">
+						<h4>Análise e Desenvolvimento de Sistemas</h4>
+						<p>Atualmente, estou cursando Análise e Desenvolvimento de Sistemas, adquirindo conhecimentos em programação, desenvolvimento de software, banco de dados, lógica de programação e arquitetura de sistemas.</p>
+						<p>Estou buscando ampliar meu conhecimento e aplicar conceitos práticos para resolver problemas reais no campo da técnologia da informação</p>
+					</div>
+				</div>
+				<h3>Cursos</h3>
+				<div class="experiencia-profissional">
+					<div class="dados">
+						<div class="icone">
+							<h2>CEC-ITECC LTDA</h2>
+                    		<i class="fas fa-circle"></i>
+                    	</div>						
+						<p>Brasília-DF</p>
+						<p>Conclusão -  set 2024</p>
+					</div>
+					<div class="profissao">
+						<h4>Técnico em Informatica</h4>
+						<p></p>
+					</div>
+				</div>
+				<div class="experiencia-profissional">
+					<div class="dados">
+						<div class="icone">
+							<h2>Udemy</h2>
+                    		<i class="fas fa-circle"></i>
+                    	</div>						
+						<p>Brasília-DF</p>
+						<p>Conclusão -  out 2024</p>
+					</div>
+					<div class="profissao">
+						<h4>Algorítimos e Lógica de Programação</h4>
+						<p></p>
+					</div>
+				</div>
+				<div class="experiencia-profissional">
+					<div class="dados">
+						<div class="icone">
+							<h2>Udemy</h2>
+                    		<i class="fas fa-circle"></i>
+                    	</div>						
+						<p>Brasília-DF</p>
+						<p>Conclusão -  nov 2024</p>
+					</div>
+					<div class="profissao">
+						<h4>PHP 8 Desenvolvimento Web</h4>
+						<p></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="certificados">
+	<div class="container">	
+		<div class="conteudo">
+			<div class="texto">
+				<h2 class="titulo center">Certificados</h2>
+			</div>
+			<div class="itens">
+				<div id="item" class="item">
+					<a href="<?=URL_BASE?>resources/imagens/certificado1.png" class="swipebox" title="Ensino Médio">
+						<div class="img">
+							<img src="<?=URL_BASE?>resources/imagens/certificado1.png">
+						</div>
+						<div class="informacoes">
+							<p class="data">01/10/2018</p>						
+							<h3 class="titulo">Ensino Médio</h3>
+							<button class="btn" type="button">Ver Certificado</button>
+						</div>
+					</a>
+				</div>
+				<div id="item" class="item">
+					<a href="<?=URL_BASE?>resources/imagens/certificado2.png" class="swipebox" title="Técnico em Informatica">
+						<div class="img">
+							<img src="<?=URL_BASE?>resources/imagens/certificado2.png">
+						</div>
+						<div class="informacoes">
+							<p class="data">19/09/2024</p>						
+							<h3 class="titulo">Técnico em Informatica</h3>
+							<button class="btn" type="button">Ver Certificado</button>
+						</div>
+					</a>
+				</div>
+				<div id="item" class="item">
+					<a href="<?=URL_BASE?>resources/imagens/certificado3.png" class="swipebox" title="Algoritimos e Lógica de Programação">
+						<div class="img">
+							<img src="<?=URL_BASE?>resources/imagens/certificado3.png">
+						</div>
+						<div class="informacoes">
+							<p class="data">22/10/2024</p>						
+							<h3 class="titulo">Algoritimos e Lógica de Programação</h3>
+							<button class="btn" type="button">Ver Certificado</button>
+						</div>
+					</a>
+				</div>
+				<div id="item" class="item">
+					<a href="<?=URL_BASE?>resources/imagens/certificado4.png" class="swipebox" title="PHP 8 Desenvolvimento Web">
+						<div class="img">
+							<img src="<?=URL_BASE?>resources/imagens/certificado4.png">
+						</div>
+						<div class="informacoes">
+							<p class="data">17/11/2024</p>						
+							<h3 class="titulo">PHP 8 Desenvolvimento Web</h3>
+							<button class="btn" type="button">Ver Certificado</button>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?=$this->fetch('commons/footer.php', $data)?>
